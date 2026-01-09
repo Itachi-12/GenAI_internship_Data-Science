31e8698bf3f6e019379403545718eb565f007779
Pavankalyan Burri <pavankalyanburri8@gmail.com>
2026-01-09 19:12:02 +0530

    Add files via upload

--- Flask/app.py
+++ Flask/app.py
@@ -1,22 +1,31 @@
-# step-1 : import your flask
+from flask import Flask, render_template, request
+import re
 
-from flask import Flask , request
+app = Flask(__name__)
 
-# step-2 : Initilaise flask object here
+@app.route("/", methods=["GET", "POST"])
+def index():
+    matches = []
+    error = None
+    test_string = ""
+    pattern = ""
 
-app=Flask(__name__)
+    if request.method == "POST":
+        test_string = request.form.get("test_string")
+        pattern = request.form.get("pattern")
 
-# step-3 : Create endpoint or route + Logic
-@app.route('/name')
-def uppercase_name():
-    name = request.args.get('name')
+        try:
+            matches = re.findall(pattern, test_string)
+        except re.error as e:
+            error = f"Invalid Regular Expression: {e}"
 
-    if not name:
-        return ' Please provide name as a query parameter.'
+    return render_template(
+        "result.html",
+        matches=matches,
+        error=error,
+        test_string=test_string,
+        pattern=pattern
+    )
 
-    return f' {name.strip().upper()}'
-
-# step4 : Run the application
-
-if __name__== '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
