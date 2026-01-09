8b9c65b07034148e188df08d934fde8f91448384
Pavankalyan Burri <pavankalyanburri8@gmail.com>
2026-01-09 23:36:54 +0530

    Update app.py

--- Flask/templates/app.py
+++ Flask/templates/app.py
@@ -9,10 +9,11 @@ def index():
     if request.method == "POST":
         note = request.form.get("note")
 
-        if note:  # avoid empty notes
+        if note: 
             notes.append(note)
 
     return render_template("home.html", notes=notes)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
